4effb47b09bd103cbe0c12cd0fca92d84b019f5d
Geoffrey Chandler <geoffc@gmail.com>
2012-05-04 22:29:49 -0700

    added rest controller

---
+++ backend/src/main/java/com/voyzala/controller/RestController.java
@@ -0,0 +1,38 @@
+package com.voyzala.controller;
+
+import com.voyzala.model.domain.Card;
+import com.voyzala.service.GameService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+
+/**
+ * description
+ * <p/>
+ * <small>
+ * Originally created by gcc on 5/4/12 at 10:18 PM
+ * </small>
+ *
+ * @author Geoffrey Chandler, geoffc@gmail.com
+ */
+@Controller
+public class RestController {
+
+    private final GameService gameService;
+
+    @Inject
+    public RestController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/game", method = RequestMethod.GET)
+    public Card startGame(@RequestParam String userId, @RequestParam String opponentId) {
+        return gameService.startRound(Long.parseLong(userId), Long.parseLong(opponentId));
+    }
+
+}
