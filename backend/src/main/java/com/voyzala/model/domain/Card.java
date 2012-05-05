212bfae0042d5a8131ef2fa793e2e1119c89623a
Geoffrey Chandler <geoffc@gmail.com>
2012-05-04 21:55:40 -0700

    added stub domain objects

---
+++ backend/src/main/java/com/voyzala/model/domain/Card.java
@@ -0,0 +1,13 @@
+package com.voyzala.model.domain;
+
+/**
+ * description
+ * <p/>
+ * <small>
+ * Originally created by gcc on 5/4/12 at 9:55 PM
+ * </small>
+ *
+ * @author Geoffrey Chandler, geoffc@gmail.com
+ */
+public class Card {
+}
